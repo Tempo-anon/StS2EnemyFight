24a00354187386f05c04f9513348c30291334e8c
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2026-08-17 00:08:55 -0400

    Add some more monsters

---
+++ README.md
@@ -0,0 +1,7 @@
+Todo list
+- [ ] Import the rest of the simple single enemy fights
+- [ ] Add in complex single enemy fights
+- [ ] Add in multi enemy fights
+- [ ] Randomized health range
+- [ ] A8 health/block values
+- [ ] A9 damage values
--- monsters.js
+++ monsters.js
@@ -8,6 +8,7 @@ export class Monster {
     this.weakTurns = 0;
     this.vulnTurns = 0;
     this.block = 0;
+    this.artifact = 0;
     }
 
     onTurn(turn, opponent, log) {
@@ -55,13 +56,23 @@ export class Monster {
     }
 
     applyWeak(opponent, log, weakAmt) {
-        opponent.weakTurns += weakAmt;
-        log.push(`${opponent.name} has become weak for ${weakAmt} turns!`)
+        if (opponent.artifact > 0) {
+            log.push(`${opponent.name}'s artifact blocked the debuff!`);
+            opponent.artifact -= 1;
+        } else {
+            opponent.weakTurns += weakAmt;
+            log.push(`${opponent.name} has become weak for ${weakAmt} turns!`)
+        }
     }
 
     applyVuln(opponent, log, vulnAmt) {
-        opponent.vulnTurns += vulnAmt;
-        log.push(`${opponent.name} has become vulnerable for ${vulnAmt} turns!`)
+        if (opponent.artifact > 0) {
+            log.push(`${opponent.name}'s artifact blocked the debuff!`);
+            opponent.artifact -= 1;
+        } else {
+            opponent.vulnTurns += vulnAmt;
+            log.push(`${opponent.name} has become vulnerable for ${vulnAmt} turns!`)
+        }
     }
 }
 
@@ -258,5 +269,101 @@ export class BygoneEffigy extends Monster {
     }
 }
 
+export class Entomancer extends Monster {
+    constructor() {
+        super("Entomancer", 145, 145, 0, "./images/StS2_Entomancer.webp");
+        this.hive = 1;
+    }
+    beeeees(opponent, log) {
+        this.multiAtk(opponent, log, 3, 7);
+    }
+    spear(opponent, log) {
+        this.attack(opponent, log, 18);
+    }
+    pheremoneSpit(log) {
+        if (this.hive == 3) {
+            this.buffStr(log, 2);
+        } else {
+            this.buffStr(log, 1);
+            this.hive += 1;
+        }
+    }
 
-export const monsterClasses = [Nibbit, FuzzyWurmCrawler, Mawler, Byrdonis, BygoneEffigy, SludgeSpinner];
+    onTurn(turn, opponent, log) {
+        if (turn % 3 == 1) {
+            this.beeeees(opponent, log);
+        } else if (turn % 3 == 2) {
+            this.spear(opponent, log);
+        } else {
+            this.pheremoneSpit(log);
+        }
+    }
+}
+
+export class InfestedPrism extends Monster {
+    constructor() {
+        super("Infested Prism", 161, 161, 0, "./images/StS2_Infested_Prism.webp");
+        this.hive = 1;
+    }
+    jab(opponent, log) {
+        this.attack(opponent, log, 15);
+    }
+    radiate(opponent, log) {
+        this.attack(opponent, log, 11);
+        this.gainBlock(log, 16);
+    }
+    whirlwind(opponent, log) {
+        this.multiAtk(opponent, log, 5, 3);
+    }
+    pulsate(opponent, log) {
+        this.attack(opponent, log, 8);
+        this.gainBlock(log, 20);
+    }
+    onTurn(turn, opponent, log) {
+        if (turn % 5 == 1) {
+            this.jab(opponent, log);
+        } else if (turn % 5 == 2) {
+            this.radiate(opponent, log);
+        } else if (turn % 5 == 3) {
+            this.whirlwind(opponent, log);
+        } else {
+            this.pulsate(opponent, log);
+        }
+    }
+}
+
+export class MechaKnight extends Monster {
+    constructor() {
+        super("Mecha Knight", 300, 300, 0, "./images/StS2_Mecha_Knight.webp");
+        this.artifact = 3;
+    }
+    charge(opponent, log) {
+        this.attack(opponent, log, 25);
+    }
+    flamethrower(opponent, log) {
+        this.attack(opponent, log, 8);
+    }
+    windup(log) {
+        this.gainBlock(log, 15);
+        this.buffStr(log, 5);
+    }
+    heavyCleave(opponent, log) {
+        this.attack(opponent, log, 35);
+    }
+    onTurn(turn, opponent, log) {
+        if (turn == 1) {
+            this.charge(opponent, log);
+        } else {
+            if (turn % 4 == 2) {
+                this.flamethrower(opponent, log);
+            } else if (turn % 4 == 3) {
+                this.windup(log);
+            } else {
+                this.heavyCleave(opponent, log);
+            }
+        }
+    }
+}
+
+
+export const monsterClasses = [Nibbit, FuzzyWurmCrawler, Mawler, Byrdonis, BygoneEffigy, SludgeSpinner, Entomancer, MechaKnight];
